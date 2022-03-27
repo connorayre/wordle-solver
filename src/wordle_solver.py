145e1b65774342911604f34ba792d469748b35d0
Zakaria Ismail <1112zakaria@gmail.com>
2022-03-27 02:31:51 -0400

    Blank wordle_solver.py file init

--- src/wordle_solver.py
+++ src/wordle_solver.py
@@ -1,8 +1,14 @@
 # ------------------------------------------------------------------------------
-# tests directory
+# wordle_solver.py
 #
 # March 2022, Connor Ayre, Tom Zhu, Zakaria Ismail
 #
 # Copyright (c) 2022
 # All rights reserved.
 # ------------------------------------------------------------------------------
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    main()
