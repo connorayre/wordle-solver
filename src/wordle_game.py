a1cf7977fc9afa8a02de4964ad8c6f8e38e4e3a9
zhu-tom <zhutom01@gmail.com>
2022-04-07 14:17:50 -0400

    fix game bugs

--- src/wordle_game.py
+++ src/wordle_game.py
@@ -3,42 +3,39 @@ from wordle_ai import LetterState
 from wordle_db import words
 
 class WordleGame:
-  def __init__(self):
-    self.__init__(words)
-
-  def __init__(self, words):
+  def __init__(self, words = words):
     self.words = words
 
   def guess_word(self, word):
-    if len(self.game_state) >= 6 or "".join([guess["letter"] for guess in self.game_state[-1]]) == self.answer:
+    if len(self.game_state) >= 6 or (len(self.game_state) > 0 and "".join([guess["letter"] for guess in self.game_state[-1]]) == self.answer):
       return
 
     letter_indices = self.letter_indices.copy()
     guess = []
     for i, c in enumerate(word):
+      colour = LetterState.EMPTY
       if c in letter_indices:
-        colour = LetterState.EMPTY
         if i in letter_indices[c]:
-          letter_indices.remove(i)
+          letter_indices[c].remove(i)
           colour = LetterState.GREEN
         else:
           colour = LetterState.YELLOW
-        guess.append({ "letter" : c, "state" : colour })
+      guess.append({ "letter" : c, "state" : colour })
     self.game_state.append(guess)
     return self.game_state
 
-  def start_game(self, answer):
-    self.answer = answer
+  def start_game(self, answer = ""):
+    if answer == "": 
+      self.answer = random.choice(self.words)
+    else:
+      self.answer = answer
     self.game_state = []
     self.letter_indices = {}
     for i, letter in enumerate(answer):
       if letter in self.letter_indices:
         self.letter_indices[letter].add(i)
       else:
-        self.letter_indices[letter] = {i}
-
-  def start_game(self):
-    self.start_game(random.choice(self.words))
+        self.letter_indices[letter] = set([i])
   
   def get_state(self):
     return self.game_state
